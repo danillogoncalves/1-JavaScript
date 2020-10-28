2f99551a3985410cace09563921ef88e963dfb92
Danillo <danillo.ooo@hotmail.com>
2020-10-28 15:31:13 -0300

    Meu Primeiro programa: trabalhando com variáveis

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Meu primeiro programa: trabalhando com variáveis");
+
+const idade = 34;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+
+console.log(idadeSomada);
