d1f8128035696817abf21e77ea0e73b902e6b79f
Danillo <danillo.ooo@hotmail.com>
2020-10-28 16:48:14 -0300

    Apagando códigos que eram de outro exercício

--- 3-operacoesAritimetricas.js
+++ 3-operacoesAritimetricas.js
@@ -3,11 +3,3 @@ console.log("Oberações Aritimétricas");
 console.log(2+2);
 console.log(10+8*2);
 console.log((10+8)*2);
-console.log("ano"+2020);
-console.log("2"+"2");
-console.log(parseInt("2")+parseInt("2"));
-console.log("10"/"2");
-console.log("Danillo"/"2"); //Not a Number
-console.log("7"/"2");
-console.log(6.5);
-console.log(6,5);
