92cf56e5ccc5c3ab35d2a92dba422af865b90a44
Danillo <danillo.ooo@hotmail.com>
2020-10-28 17:07:29 -0300

    Trabalhando com atribuição de variáveis

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,9 @@
+console.log("Trabalhando com atribuição de variáveis")
+const idade = 29;
+const primeiroNome = "Danillo";
+const sobrenome = "Gonçalves Batista";
+
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
