b7dc4ac990c4634d056112d062e2901901a74c61
Danillo <danillo.ooo@hotmail.com>
2020-10-29 14:35:28 -0300

    Acrescentando o exemplo para o uso de LET, e o uso da variável nomeCompleto

--- 5-atribuicaoDeVariaveis.js
+++ 5-atribuicaoDeVariaveis.js
@@ -3,7 +3,12 @@ const idade = 29;
 const primeiroNome = "Danillo";
 const sobrenome = "Gonçalves Batista";
 
+//Esse é uma exemplo de como pode deixar uma variável editavel usando LET
+let contador = 0;
+contador = contador + 1;
+
 console.log(primeiroNome, sobrenome);
 console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 
 const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
